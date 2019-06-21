5828dfd83307071544835038817de51b7581d401
solonkoolesya <olesol22@gmail.com>
2019-06-21 16:54:35 +0300

    second homework

--- homework/homework02.py
+++ homework/homework02.py
@@ -1,63 +1,27 @@
+
 from pprint import pprint
-
-class XO:
-    X, O, _ = 1, 0, None
-
-    def slice(board):
-        return((board[:3], board[3:6], board[6:9], board[::3], board[1::3], board[2::3],  board[::4], board[2:7:2] ))
-
-
-    def outcome(board):
-        for i in board:            
-            if None in i:
-                return(2)
-            elif len(set(i)) == 1 and i[0] == 1:
-                return(1)			
-            elif len(set(i)) == 1 and i[0] == 0:
-                return(0)
-            else:
-               pass
-        return(3) 
-
-test_board1 = (0, 0, 0
-               1, 1, 0
-			   0, 1, 0)
-
-test_board2 = (1, 0, 1,
-               1, 1, 0, 
-               1, 0, 0)
-
-test_board3 = (1, 0, 1,
-               0, None, 0, 
-               1, 0, 0)
-
-test_board4 = (1, 0, 1,
-               0, 1, 0, 
-               0, 1, 0)
-
-print('board1')
-pprint(slice(test_board1))
-pprint(outcome(slice(test_board1)))
-
-print('board2')
-pprint(slice(test_board2))
-pprint(outcome(slice(test_board2)))
-
-print('board3')
-pprint(slice(test_board3))
-pprint(outcome(slice(test_board3)))
-
-print('board4')
-pprint(slice(test_board4))
-pprint(outcome(slice(test_board4)))
-
-
 X, O, _ = 1, 0, None
+#O_WINS, X_WINS, UNDEFINED, DRAW = range(4)
 
-TEST_BOARD = (
-    O, X, O,
-    X, O, X,
-    _, _, X
-)
+def slice(board):
+    return([board[:3], board[3:6], board[6:9], board[::3], board[1::3], board[2::3],  board[::4], board[2:7:2] ])
 
-O_WINS, X_WINS, UNDEFINED, DRAW = range(4)
+def outcome(board):
+    if_none_flag = 0
+    for i in board:            
+        if None in i:
+            if_none_flag += 1
+        elif len(set(i)) == 1 and i[0] == 1:
+            return(1, 'X_WINS')			
+        elif len(set(i)) == 1 and i[0] == 0:
+            return(0, 'O_WINS')
+    if if_none_flag > 0:
+      return(2, 'UNDEFINED')
+    else:
+      return(3, 'DRAW') 
+
+a = (O, X, X, 
+     X, O, O, 
+     O, _, X)
+pprint(a)
+print(outcome(slice(a)))
